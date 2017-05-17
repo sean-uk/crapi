3d79aed1b8d58531abc1d1b48f10a6437a09d2b6
sean.james <sean.james@twentysixdigital.com>
2017-05-17 14:57:22 +0100

    add some more SecretHeaderMiddlewareTest cases

--- src/App/src/Tests/Middleware/SecretHeaderMiddlewareTest.php
+++ src/App/src/Tests/Middleware/SecretHeaderMiddlewareTest.php
@@ -16,16 +16,73 @@ use App\Middleware\SecretHeaderMiddleware;
 
 class SecretHeaderMiddlewareTest extends TestCase
 {
+    /** @var SecretHeaderMiddleware $middleware */
+    private $middleware;
+
+    /** @var RequestInterface $request */
+    private $request;
+
+    /** @var DelegateInterface $delegate */
+    private $delegate;
+
+    /**
+     * Create a standard SUT and dependency mocks.
+     */
+    public function setUp()
+    {
+        $this->middleware = new SecretHeaderMiddleware();
+        $this->request = Phake::mock(ServerRequestInterface::class);
+        $this->delegate = Phake::mock(DelegateInterface::class);
+    }
+
     public function testNoAuthHeader()
     {
         // if the special 'Auth' header is not set, you'd not expect any modified behaviour
-        $middleware = new SecretHeaderMiddleware();
+        $this->middleware->process($this->request, $this->delegate);
 
-        $request = Phake::mock(ServerRequestInterface::class);
-        $delegate = Phake::mock(DelegateInterface::class);
+        // verify the request has been passed on, that the middleware hasn't intervened.
+        Phake::verify($this->delegate)->process($this->request);
+        Phake::verifyNoFurtherInteraction($this->request);
+    }
 
-        $middleware->process($request, $delegate);
+    public function testAuthHeaderEmpty()
+    {
+        // the header must have the string value 'false' to have any effect
+        Phake::when($this->request)
+            ->getHeader('Auth')
+            ->thenReturn([]);
 
-        Phake::verify($delegate)->process($request);
+        $this->middleware->process($this->request, $this->delegate);
+
+        // verify the request has been passed on, that the middleware hasn't intervened.
+        Phake::verify($this->delegate)->process($this->request);
+        Phake::verifyNoFurtherInteraction($this->request);
+    }
+
+    public function testAuthHeaderMisspelling()
+    {
+        // the header must have the exact string value 'false' to have any effect
+        Phake::when($this->request)
+            ->getHeader('Auth')
+            ->thenReturn(['False']);
+
+        $this->middleware->process($this->request, $this->delegate);
+
+        Phake::verify($this->delegate)->process($this->request);
+        Phake::verifyNoFurtherInteraction($this->request);
+    }
+
+    public function testAuthHeaderDeny()
+    {
+        // correct value, right case etc.
+        Phake::when($this->request)
+            ->getHeader('Auth')
+            ->thenReturn(['false']);
+
+        $response = $this->middleware->process($this->request, $this->delegate);
+
+        // verify that the request is NOT delegated. And that something else is returned.
+        Phake::verify($this->delegate, Phake::never())->process($this->request);
+        $this->assertNotEquals($this->request, $response);
     }
 }
